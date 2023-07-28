fca0666ed3fd2a88ee2e3638d54beabe85a5a95d
adam-horvath8 <adam.horvath16299@gmail.comm>
2023-07-28 19:21:10 +0200

    Add minor style changes

--- src/img-sources.js
+++ src/img-sources.js
@@ -1,5 +1,4 @@
 import overcastCloud from "./images/amcharts_weather_icons_1.0.0/animated/cloudy-day-1.svg";
-
 import clearSky from "./images/amcharts_weather_icons_1.0.0/animated/day.svg";
 import rain from "./images/amcharts_weather_icons_1.0.0/animated/rainy-1.svg";
 import thunderstorm from "./images/amcharts_weather_icons_1.0.0/animated/thunder.svg";
